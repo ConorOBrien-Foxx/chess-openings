fc6435d389a232503d67f15e042c304abcaa4e45
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2025-12-11 00:32:24 -0500

    add basic framework of app

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # chess-openings
 A simplified view of the lichess chess openings
+
+## Cloning
+
+This uses [git submodules](https://git-scm.com/book/en/v2/Git-Tools-Submodules); you may wish to `git clone --recurse-submodules`.
---
+++ openings.js
@@ -0,0 +1,119 @@
+const Directory = [
+    "./lichess-openings/a.tsv",
+    "./lichess-openings/b.tsv",
+    "./lichess-openings/c.tsv",
+    "./lichess-openings/d.tsv",
+    "./lichess-openings/e.tsv",
+];
+
+const parseTsv = tsv => tsv.trim().split("\n").map(line => line.trim().split("\t"));
+
+let scriptsDownloaded = 0;
+const parseDirectory = path =>
+    fetch(path)
+        .then(req => req.text())
+        .then(text => {
+            scriptsDownloaded++;
+            let status = document.querySelector(".status");
+            if(status) {
+                status.textContent = `${scriptsDownloaded}/${Directory.length} downloaded…`;
+            }
+            return text;
+        });
+
+Promise.all([
+    Promise.all(Directory.map(parseDirectory)),
+    new Promise(resolve => window.addEventListener("load", resolve))
+]).then(([tsvs]) => {
+    let bigTsv = tsvs.flatMap(tsv => parseTsv(tsv).slice(1));
+    
+    let status = document.querySelector(".status");
+    let openings = document.querySelector(".openings");
+    let viewButton = document.querySelector(".view-button");
+    let lichessLink = document.querySelector(".lichess-link");
+    let filterText = document.querySelector(".filter");
+    
+    bigTsv.forEach(cols => {
+        let tr = document.createElement("tr");
+        let ecoTd = document.createElement("td");
+        let nameTd = document.createElement("td");
+        let pgnTd = document.createElement("td");
+        
+        let [ eco, name, pgn ] = cols;
+        
+        ecoTd.textContent = eco;
+        let [ whole, family, variation, subvariations ] = name.match(/^(.+?)(?:: (.+?)((?:, .+?)+)?)?$/);
+        // console.log(family, variation, subvariations);
+        
+        let familyName = document.createElement("span");
+        familyName.textContent = family;
+        familyName.classList.add("family");
+        nameTd.appendChild(familyName);
+        if(variation) {
+            familyName.textContent += ": ";
+            let variationEl = document.createElement("span");
+            variationEl.classList.add("variation");
+            variationEl.textContent = variation;
+            nameTd.appendChild(variationEl);
+        }
+        if(subvariations) {
+            let subvariationsEl = document.createElement("span");
+            subvariationsEl.classList.add("subvariations");
+            subvariationsEl.textContent = subvariations;
+            nameTd.appendChild(subvariationsEl);
+        }
+        
+        pgnCode = document.createElement("code");
+        pgnCode.textContent = pgn;
+        pgnTd.appendChild(pgnCode);
+        
+        tr.appendChild(ecoTd);
+        tr.appendChild(nameTd);
+        tr.appendChild(pgnTd);
+        openings.appendChild(tr);
+    });
+    
+    openings.addEventListener("click", ev => {
+        let parentTr = ev.target.closest("tr");
+        if(!parentTr) {
+            console.info("Rejecting click handler for:", ev.target);
+            return;
+        }
+        openings.querySelectorAll("tr").forEach(tr =>
+            tr !== parentTr && tr.classList.remove("focused"));
+        let isFocused = parentTr.classList.toggle("focused");
+        viewButton.disabled = !isFocused;
+    });
+    viewButton.disabled = true; // reset any potentially saved state
+    
+    viewButton.addEventListener("click", () => {
+        let pgn = document.querySelector(".focused code");
+        if(!pgn) {
+            console.warn("No pgn found when clicking view button");
+            return;
+        }
+        pgn = pgn.textContent;
+        let encoded = pgn.replace(/\d+\./g, "").trim().replace(/\s+/g, "_");
+        let url = `https://lichess.org/analysis/pgn/${encoded}`;
+        lichessLink.href = url;
+        lichessLink.click();
+    });
+    
+    const normalize = text => text.toLowerCase().replace(/\s+/g, "");
+    const normalizedCache = [];
+    const syncFilter = () => {
+        let filter = normalize(filterText.value);
+        let idx = 0;
+        for(let tr of openings.children) {
+            normalizedCache[idx] ??= normalize(tr.textContent);
+            tr.classList.toggle("hidden", !normalizedCache[idx].includes(filter));
+            idx++;
+        }
+    };
+    filterText.addEventListener("input", syncFilter);
+    syncFilter();
+    
+    status.textContent = "Page loaded.";
+    setTimeout(() => status.textContent = "", 1000);
+    console.log(bigTsv);
+});
